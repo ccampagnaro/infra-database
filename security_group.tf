8ef7a40d983dddd34ad1f0345e4ba9400c3ff160
ccampagnaro <48631535+ccampagnaro@users.noreply.github.com>
2024-12-01 20:04:18 -0300

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "Allow MySQL traffic"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
