be5d7dea222a945b0e24d597e4a411c3efeb7da4
ccampagnaro <48631535+ccampagnaro@users.noreply.github.com>
2024-12-01 21:04:31 -0300

    Update variables.tf
    Inclusão de novas variaveis

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "region" {
   description = "AWS region"
   type        = string
